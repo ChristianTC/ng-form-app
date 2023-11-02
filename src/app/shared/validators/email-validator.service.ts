ed81838472a0748f5f32d6d7a8f688fb1bd72221
ChristianTC <christiantambo15@gmail.com>
2023-11-02 16:41:44 -0400

    *create email validator with Observable

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -8,13 +8,33 @@ export class EmailValidator implements AsyncValidator {
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
 
-    console.log({email});
+    const httpCallObsevable = new Observable<ValidationErrors | null>((subscriber)=>{
+      console.log(email);
+      if(email === 'c@c.com'){
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+      }
 
-    return of({
-      emailTaken: true
+      subscriber.next(null);
+      subscriber.complete();
     }).pipe(
-      delay(2000)
-    )
+      delay(3000)
+    );
+
+    return httpCallObsevable
+
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+
+  //   console.log({email});
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+  // }
 
 }

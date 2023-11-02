cd5b86ab472ac0c07f3021ebfbe28137d1c7ae57
ChristianTC <christiantambo15@gmail.com>
2023-11-02 11:33:19 -0400

    reset values in Form; also using ngOnInit

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const i500 = {
+  name: 'i500+',
+  price: 5000,
+  inStorage: 6,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl('', [], []),
@@ -22,9 +28,14 @@ export class BasicPageComponent {
 
   constructor(  private fb: FormBuilder ){}
 
+  ngOnInit(): void {
+    this.myForm.reset(i500)
+  }
+
   onSave(): void {
     if(this.myForm.invalid) return
     console.log(this.myForm.value);
+    this.myForm.reset({price: 0, inStorage: 0});
   }
 
 }
